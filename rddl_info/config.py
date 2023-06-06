0caea42c603afd45a211ccad5ce0a2fd447d2e3e
Julian Strobl <jmastr@mailbox.org>
2023-06-06 10:05:01 +0200

    add node7 and change port of node15
    Signed-off-by: Julian Strobl <jmastr@mailbox.org>

--- rddl_info/config.py
+++ rddl_info/config.py
@@ -13,11 +13,12 @@ RDDL_NODES = config(
     {"uri":"http://node4-rddl-testnet.twilightparadox.com:9984","pub":"9tZ3twvWYeELvbdSYR9FdQJM3tYu9iRQ4u6nnqqfQ62v"},\
     {"uri":"http://node5-rddl-testnet.chickenkiller.com:9984","pub":"2eBpuzzscZVGgmxfCH9DakpH4zAZSXYZLnrs6tSgBwsK"},\
     {"uri":"http://node6-rddl-testnet.twilightparadox.com:9984","pub":"8eGw9CVTXBYh4RjTsYFWa8LVaKkwBVm8tD1ekFrnjYMD"},\
+    {"uri":"http://node7-rddl-testnet.twilightparadox.com:9984","pub":"9QKm6R5Yf8aCKjbQU795CWnCqbp76NRfUa4ZiNhpdJV6"},\
     {"uri":"http://node8-rddl-testnet.twilightparadox.com:9984","pub":"CmX7akU8G5tiQfxgxYykGeiYAHTLa4bs3A5pKf1xVKHz"},\
     {"uri":"http://node9-rddl-testnet.twilightparadox.com:9984","pub":"8Tr71EWFVAB6TnQUxhzUPwh3EyZLN1YyNxd4y2fnpTDC"},\
     {"uri":"http://node10-rddl-testnet.twilightparadox.com:9984","pub":"FsP7WT12wkMKeEK1YU2EQ3cVKtarMTGVmVQUyxCBpBJy"},\
     {"uri":"http://node12-rddl-testnet.twilightparadox.com:9984","pub":"Fjj1yu1uXhsmq89qJU5tYqXfMPaZsA3K95xHCkvrcEUM"},\
     {"uri":"http://node14-rddl-testnet.twilightparadox.com:9994","pub":"4zwmCxhVEsmNko8KofkzxTrfjnRPoDXowGWJRPm5refo"},\
-    {"uri":"http://node15-rddl-testnet.twilightparadox.com:10004","pub":"G8HWRJBq2jAdKrf5vrqTB6uSnYnQa61QenXfVbaWtATh"}\
+    {"uri":"http://node15-rddl-testnet.twilightparadox.com:9984","pub":"G8HWRJBq2jAdKrf5vrqTB6uSnYnQa61QenXfVbaWtATh"}\
     ]',
 )
